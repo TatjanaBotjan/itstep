fe4494de19e774065f89cdc8cbc885eae2c10a4d
Tatiana Botian <tatjanab6@rambler.ru>
2022-10-05 23:49:06 +0300

    Tasks23

---
+++ Tasks24/app.js
@@ -0,0 +1,24 @@
+var a, b, c, d, e;
+a = 18;
+b = 30;
+c = 21;
+d = 20;
+var e = a * 3600 + b * 60;
+var f = c * 3600 + d * 60;
+var j = f - e;
+console.log(j);
+
+// периментр
+
+var a = 3, b = 1;
+var c = (a + b) * 2;
+
+// площадь
+var d = a * b;
+
+// отношение
+var e = c / d;
+console.log(e);
+
+//деньги
+var money = 1000, base = 1000, procent = 10;
