694aa33efef0352a05ba8eb78dba1664dc2c4de2
Tatiana Botian <tatjanab6@rambler.ru>
2022-11-30 22:46:01 +0300

    Tasks37

--- Tasks37/app.js
+++ Tasks37/app.js
@@ -35,6 +35,22 @@ console.log(newArray);
 console.log("Task 4:");
 // 4. Проверить 2 массива на полное совпадение
 
+a = [1, 2, 3, 2];
+b = [1, 2, 3, 1];
+
+if (a.length == b.length) {
+    let isEqual = a.every(function (item, index) {
+        return a[index] == b[index]; // item == a[index]
+    });
+    if (isEqual) {
+        console.log("совпадают");
+    } else {
+        console.log("не совпадают");
+    }
+} else {
+    console.log("не совпадают");
+}
+
 
 console.log("Task 5");
 // 5. Дан массив [1,3,6,4,3,3,5,6]. Исключить из него значения из массива [1,3]. Должно работать для любых 2 аналогичных массивов
@@ -63,3 +79,46 @@ c = a.filter(function (item) {
 
 console.log(sum);
 console.log(c);
+
+console.log("Task 7");
+// 7. Дано предложение на русском языке.Определить, является ли оно панграммой
+
+let str = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+let arrRu = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'];
+
+
+
+let newStr = str.toLowerCase().split("").sort().join("");
+let newarrRu = arrRu.sort().join("");
+
+if (newStr == newarrRu) {
+    console.log("Предложение является панграмма");
+} else {
+    console.log("Предложение не является панграммой");
+}
+
+
+console.log("Task 8");
+// 8. Дано натурально число.Получить число, которое образуется из записи цифр данного в обратном порядке
+
+myArray = [123];
+
+newArray = myArray.toString().split("").reverse().join("");
+
+let num = Number(newArray);
+
+console.log(num)
+
+console.log("Task 9");
+// 9. Дан массив и число A.Переставить элементы в массиве так, чтобы сначала стояли элементы меньшие числа A, а потом большие
+
+array = [1, 5, 9, 2, 3, 7, 15, 25];
+num = 9;
+
+
+let array1 = array.filter((item) => item <= num).sort((a, b) => a - b);
+let array2 = array.filter((item) => item > num).sort((a, b) => a - b);
+
+newArray = array1.concat(array2);
+
+console.log(array1, array2, newArray);
