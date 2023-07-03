164cf3426db824387dadbe9477c969e92ff261f1
Tatiana Botian <tatjanab6@rambler.ru>
2023-07-03 22:51:28 +0300

    final 03.07

--- FINAL/js/consultation.js
+++ FINAL/js/consultation.js
@@ -22,7 +22,7 @@ closeBtn.addEventListener("click", function (event) {
 });
 
 
-let programBtn = document.querySelector(".program__row_consultation_btn");
+let programBtn = document.querySelector(".program__row_block_consultation_btn");
 let programConsultation = document.querySelector(".hide");
 
 programBtn.addEventListener("click", function (event) {
@@ -42,5 +42,55 @@ hideBtn.addEventListener("click", function (event) {
         programConsultation.classList.remove("unhide");
     } else {
         programConsultation.classList.add("hide");
+    }
+});
+
+//*индивидуальная консультация*//
+
+let individualBtn = document.querySelector(".individual-consultation__row_services_individual_btn");
+let  individualConsultation = document.querySelector(".vs");
+
+individualBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (individualConsultation.classList.contains("vs")) {
+        individualConsultation.classList.remove("vs");
+    } else {
+        individualConsultation.classList.add("invs");
+    }
+});
+
+let vsBtn = document.querySelector(".form-individual_close_btn");
+
+vsBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (individualConsultation.classList.contains("invs")) {
+        individualConsultation.classList.remove("invs");
+    } else {
+        individualConsultation.classList.add("vs");
+    }
+});
+
+//*сопровождение*//
+
+let escortBtn = document.querySelector(".individual-consultation__row_services_personal_btn");
+let escortConsultation = document.querySelector(".escort-vs");
+
+escortBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (escortConsultation.classList.contains("escort-vs")) {
+        escortConsultation.classList.remove("escort-vs");
+    } else {
+        escortConsultation.classList.add("escort-invs");
+    }
+});
+
+let escortvsBtn = document.querySelector(".form-escort_close_btn");
+
+escortvsBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (escortConsultation.classList.contains("escort-invs")) {
+        escortConsultation.classList.remove("escort-invs");
+    } else {
+        escortConsultation.classList.add("escort-vs");
     }
 });
